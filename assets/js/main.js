ca9da2569e0448161ef78657e0c141c2fc9183d1
kjuraszek <donkrissocorleone@gmail.com>
2019-10-09 18:05:55 +0200

    Update main.js
    Code refactorization

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,65 +4,68 @@ jQuery( document ).ready(function() {
 
 	jQuery(document).mousedown(function(e){ 
 		if( e.button == 2 ) { 
-		  alert("Please, don't use right mouse button!"); 
-		  return false; 
+			alert("Please, don't use right mouse button!"); 
+			return false; 
 		} 
 		return true;
-    });
+	});
+	
 	printLetters();
+
 	jQuery(".x-img").click(function() {
 		jQuery(".banner").fadeOut(1200);
 		jQuery(".under").fadeIn(1200).css({"z-index":"1"});
 		jQuery(".banner").delay(1200).queue(function(){jQuery(this).detach()});
 		jQuery(window).resize(printLetters);
 		
-		var toggleLettersInterval = window.setInterval(toggleLetters,1);
-		var toggleRowsInterval = window.setInterval(toggleRows,1000);
+		window.setInterval(toggleLetters,1);
+		window.setInterval(toggleRows,1000);
 		jQuery('body').css('cursor', 'none');
 		document.title = setTitle()+' !';
 	});
+
 	
-	
-	});
+});
 
 function printLetters(){
+	// prints rows with  random characters
 	jQuery('.under').html('');
-	var docWidth = jQuery(document).width();
-	var docHeight = jQuery(document).height();
+	var docWidth, docHeight, fontWidth, rowHeight, letters, rows, rowContent, indexOfPortfolio, randomChar;
+	docWidth = jQuery(document).width();
+	docHeight = jQuery(document).height();
 	if(docWidth < 575) {
-		var fontWidth = 30;
-		var rowHeight = 50;
+		fontWidth = 30;
+		rowHeight = 50;
 	} else if(docWidth < 768) {
-		var fontWidth = 20;
-		var rowHeight = 40;
-	}else if(docWidth < 992) {
-		var fontWidth = 15;
-		var rowHeight = 30;
-	}else {
-		var fontWidth = 9;
-		var rowHeight = 18;
+		fontWidth = 20;
+		rowHeight = 40;
+	} else if(docWidth < 992) {
+		fontWidth = 15;
+		rowHeight = 30;
+	} else {
+		fontWidth = 9;
+		rowHeight = 18;
 	}
 
-	var letters=Math.floor(docWidth/fontWidth) - 1;
-	var rows = Math.floor(docHeight/rowHeight);
-	for (var i=0;i<rows;i++){
-		var rowContent='';
-		if( i == Math.floor(rows/2)){
-			var h = Math.floor(letters/2) - 4;
-			for (var j=0;j<letters;j++){
-				var x = String.fromCharCode(Math.floor((Math.random() * 100) + 600));
-				if(j < h || j > h + 8){
-					rowContent+='<span class="letter">'+x+'</span>';
-				} else if(j == h){
-					rowContent+='<a id="portfolio-link" href="https://portfolio.kjuraszek.pl"><span class="portfolio-letter">P</span><span class="portfolio-letter">O</span><span class="portfolio-letter">R</span><span class="portfolio-letter">T</span><span class="portfolio-letter">F</span><span class="portfolio-letter">O</span><span class="portfolio-letter">L</span><span class="portfolio-letter">I</span><span class="portfolio-letter">O</span></a>';
+	letters = Math.floor(docWidth/fontWidth) - 1;
+	rows = Math.floor(docHeight/rowHeight);
+	for (var i = 0; i < rows; i++){
+		rowContent = '';
+		if(i == Math.floor(rows/2)){
+			indexOfPortfolio = Math.floor(letters/2) - 4;
+			for (var j = 0; j < letters; j++){
+				randomChar = String.fromCharCode(Math.floor((Math.random() * 100) + 600));
+				if(j < indexOfPortfolio || j > indexOfPortfolio + 8){
+					rowContent += '<span class="letter">' + randomChar + '</span>';
+				} else if(j == indexOfPortfolio){
+					rowContent += '<a id="portfolio-link" href="https://portfolio.kjuraszek.pl"><span class="portfolio-letter">P</span><span class="portfolio-letter">O</span><span class="portfolio-letter">R</span><span class="portfolio-letter">T</span><span class="portfolio-letter">F</span><span class="portfolio-letter">O</span><span class="portfolio-letter">L</span><span class="portfolio-letter">I</span><span class="portfolio-letter">O</span></a>';
 				}
 				
 			}
-		} else{
-			
-			for (var j=0;j<letters;j++){
-				var x = String.fromCharCode(Math.floor((Math.random() * 100) + 600));
-				rowContent+='<span class="letter">'+x+'</span>';
+		} else {
+			for (var j = 0; j < letters; j++){
+				randomChar = String.fromCharCode(Math.floor((Math.random() * 100) + 600));
+				rowContent += '<span class="letter">' + randomChar + '</span>';
 			}
 		}
 		jQuery('.under').append('<div class="my-row">'+rowContent+'</div>');
@@ -72,7 +75,7 @@ function printLetters(){
 }
 
 function refreshMouseEvents(){
-
+	// attaches mouseover and mouseout event handlers to .letter
 	jQuery("body").on("mouseover", "span.letter", function() {
 		jQuery( this ).css({"color":"red","background":"red"});
 	});
@@ -80,15 +83,21 @@ function refreshMouseEvents(){
 		jQuery( this ).css({"color":"","background":""});
 	});
 }
+
 function toggleLetters(){
-	var len = jQuery('.letter').length - 1;	
-	var num = Math.floor((Math.random() * len));	
-	var el = '.letter:eq('+String(num)+')';
+	// toggles color of random .letter
+	var len, num, el;
+	len = jQuery('.letter').length - 1;	
+	num = Math.floor((Math.random() * len));	
+	el = '.letter:eq('+String(num)+')';
 	jQuery(el).toggleClass("green");
 }
+
 function toggleRows(){
-	var len = jQuery('.my-row').length - 1;	
-	var num = Math.floor((Math.random() * len));
+	// toggles background color of random .my-row
+	var len, num;
+	len = jQuery('.my-row').length - 1;	
+	num = Math.floor((Math.random() * len));
 	for(var i = 0; i < len; i++){
 		if(i == num){
 			jQuery('.my-row:eq('+String(i)+') .letter').css({"color":"black","background":"red"});
@@ -97,7 +106,9 @@ function toggleRows(){
 		}
 	}
 }
+
 function setTitle(){
+	// sets random title tag
 	var title='';
 	for(var i=0;i<13;i++){
 		title+=String.fromCharCode(Math.floor((Math.random() * 100) + 600));
